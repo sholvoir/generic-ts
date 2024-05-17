ec6e5fab0330c957886988d885f653298e6cf573
sholvoir <sovar.he@gmail.com>
2024-05-17 13:49:54 -0400

    test

--- jwt.test.ts
+++ jwt.test.ts
@@ -1,7 +1,7 @@
 import { JWT } from "./jwt.ts";
 
 Deno.test('Create Token', async () => {
-    const jwt = await new JWT({ iss: "sholvoir.com", sub: "app" }).init();
+    const jwt = new JWT({ iss: "sholvoir.com", sub: "app" });
     console.log(`key: ${await jwt.exportKey()}`);
     const token = await jwt.createToken(50 * 365 * 24 * 60 * 60);
     console.log(`token: ${token}`);
