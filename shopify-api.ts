8d7bd3eea99250fd132757e6922474e773df10e5
sholvoir <sovar.he@gmail.com>
2024-06-28 21:42:01 -0400

    jsr

--- shopify-api.ts
+++ shopify-api.ts
@@ -1,5 +1,5 @@
 // deno-lint-ignore-file no-explicit-any
-import { HTTPMethod } from "./http-method.ts";
+import type { HTTPMethod } from "./http-method.ts";
 
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
--- trans-case.test.ts
+++ trans-case.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.137.0/testing/asserts.ts";
+import { assertEquals } from "@std/assert";
 import {
     camel2pascal, camel2snake, camel2kebab, pascal2snake, pascal2kebab, snake2kebab,
     pascal2camel, snake2camel, kebab2camel, snake2pascal, kebab2pascal, kebab2snake
