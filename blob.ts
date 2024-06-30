ca3b69053578321e42cd0fa5e5e7aee19aa67bb3
sholvoir <sovar.he@gmail.com>
2024-06-29 23:17:36 -0400

    blob 2

--- blob.ts
+++ blob.ts
@@ -1,10 +1,10 @@
-export const blobToBase64 = (blob: Blob) => new Promise<string>((resolve) => {
+export const blobToBase64 = (blob: Blob): Promise<string> => new Promise<string>((resolve) => {
     const reader = new FileReader();
     reader.readAsDataURL(blob);
     reader.onloadend = () => resolve(reader.result as string);
 });
 
-export const urlToDataUrl = async (url: string) => {
+export const urlToDataUrl = async (url: string): Promise<string|undefined> => {
     const resp = await fetch(url);
     if (!resp.ok) return undefined;
     return await blobToBase64(await resp.blob());
